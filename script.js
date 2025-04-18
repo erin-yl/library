788aa48a0b3472cca6a9e83d733ec394a285163f
erin-yl <erinyinling@gmail.com>
2025-04-18 11:27:24 -0700

    Add exercise files

---
+++ script.js
@@ -0,0 +1,14 @@
+function Book(title, author, pages) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+  }
+}
+
+console.log(theHobbit.info());
