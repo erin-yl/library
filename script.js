e83814e61c3455cacf8de4753b3e1cf3fd2a6ce6
erin-yl <erinyinling@gmail.com>
2025-04-18 14:24:16 -0700

    Add arrays and constructor

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-function Book(title, author, pages) {
+const container = document.querySelector("#container");
+const button = document.querySelector(".button");
+
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
   if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor");
   }
+
+  this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-  }
 }
 
-console.log(theHobbit.info());
+function addBookToLibrary(title, author, pages, read) {
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+}
+
+function displayBook() {
+  for (el in myLibrary) {
+    console.log(el);
+  }
+  container.appendChild(myLibrary[el]);
+}
